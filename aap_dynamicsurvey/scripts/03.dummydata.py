5504584f2f535ddbb14f2679d931f1d56aca6101
ldileona <ldileona@redhat.com>
2024-05-16 16:07:42 +0200

    update

---
+++ aap_dynamicsurvey/scripts/03.dummydata.py
@@ -0,0 +1,14 @@
+# save this as generate_hosts.py
+import json
+
+os = [
+      "Fedora",
+      "CEntOS",
+      "RHEL",
+      "Ten",
+      "Eleven",
+      "Seven",
+      "any"
+]
+
+print(json.dumps(os))
